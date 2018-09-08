e702abc3b9ec7679cc027e1639fbea7921321589
Jim McManus <jmpmcmanus@flatapps.net>
2018-09-08 11:41:44 -0400

    Corrected 2016 mrent data using contract rent instead of gross rent

--- datahub_be_app/views.py
+++ datahub_be_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, permissions
-from .serializers import LTDBACS_trts_7016Serializer,bgs9800Serializer,bgs1318Serializer, durhamhdsSerializer, muniboundariesSerializer, cntyboundariesSerializer
-from .models import LTDBACS_trts_7016,bgs9800,bgs1318,durhamhds, muniboundaries, cntyboundaries
+from .serializers import LTDBACS_trts_7016Serializer,bgs9800Serializer,bgs1318Serializer,durhamhdsSerializer, muniboundariesSerializer, cntyboundariesSerializer
+from .models import LTDBACS_trts_7016,bgs9800,bgs1318,durhamhds,muniboundaries,cntyboundaries
 
 def test_view(request):
     return render(request, 'test.html')
